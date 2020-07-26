8b97963667ceffd43fe8396b2ac4141472590ed5
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-25 23:03:06 -0700

    imported orm file into course.js to create functions for database

--- models/course.js
+++ models/course.js
@@ -1,5 +1,26 @@
 var orm = require("../config/orm.js");
 
 
+var course = {
+
+    all: function(cb) {
+      orm.selectAll("golf_courses", function(res) {
+        cb(res);
+      });
+    },
+    
+    insertOne: function(cols, vals, cb) { 
+      orm.create("golf_courses", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+  
+    updateOne: function(objColVals, condition, cb) {
+      orm.update("golf_courses", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+};
+
 
 module.exports = course;
