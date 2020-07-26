7becc772feb386245499ca0b0f70876f3c460b71
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-26 00:48:43 -0700

    created routes in controllers.js file

--- controllers/golf-courses_controller.js
+++ controllers/golf-courses_controller.js
@@ -4,7 +4,44 @@ var router = express.Router();
 
 var course = require("../models/course.js");
 
-
+// // Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    course.all(function(data) {
+      var hbsObject = {
+        golf_courses: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+});
+  
+router.post("/api/golf_courses", function(req, res) { 
+    course.create([
+      "courseName", "played"
+    ], [
+      req.body.courseName, req.body.played
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+});
+  
+router.put("/api/golf_courses/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    course.update({
+      played: req.body.played
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+});
 
 
 
