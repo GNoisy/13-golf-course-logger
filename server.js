cf45194c7426de95eed86c8a0fa81598623c7a8b
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-25 22:37:15 -0700

    added routes var to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var exphbs = require("express-handlebars");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -13,6 +13,10 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
 
 app.listen(PORT, function () {
   // Log (server-side) when our server has started
